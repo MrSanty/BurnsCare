ef2721dff9b837442b5a54da2f7d53b337a727d4
MrSanty <sr.santyquinteror@gmail.com>
2024-02-03 23:54:32 -0500

    Add SplashScreen to React Native app

--- android/app/src/main/java/com/burnscare/MainActivity.kt
+++ android/app/src/main/java/com/burnscare/MainActivity.kt
@@ -1,16 +1,15 @@
 package com.burnscare
 
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
-import android.os.Bundle
 import org.devio.rn.splashscreen.SplashScreen
 
 class MainActivity : ReactActivity() {
-
-  override fun onCreate(savedInstanceState: android.os.Bundle?) {
-    SplashScreen.show(this, R.style.SplashScreenTheme)
+  override fun onCreate(savedInstanceState: Bundle?) {
+    SplashScreen.show(this)
     super.onCreate(savedInstanceState)
   }
 
