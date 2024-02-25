63a347f4fe8d5546e285f6eba653ebb6ab2f4e3a
MrSanty <sr.santyquinteror@gmail.com>
2024-02-25 16:21:44 -0500

    Refactor Exam component styling

--- src/screens/Exam.tsx
+++ src/screens/Exam.tsx
@@ -1,9 +1,38 @@
-import { View, Text } from 'react-native'
-const Exam = () => {
+import { FC } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { useNavigationChange } from 'src/hooks/useNavigationChange';
+
+interface Props {
+  navigation: any;
+}
+
+const Exam: FC<Props> = ({ navigation }) => {
+  useNavigationChange(navigation, 9);
+
   return (
-    <View>
-      <Text>Exam</Text>
+    <View style={styles.container}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>EXAMEN</Text>
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  titleContainer: {
+    padding: 20,
+    alignItems: 'center',
+    marginBottom: 30
+  },
+  title: {
+    fontFamily: 'Poppins-Bold',
+    fontSize: 22,
+    textAlign: 'center',
+    color: '#FB0263'
+  }
+})
+
 export default Exam
